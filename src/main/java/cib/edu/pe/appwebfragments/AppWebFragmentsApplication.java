5e83d900445407b27d4cfadb7390d94c82afaef7
AlexGVP <i202122156@cibertec.edu.pe>
2023-10-01 09:53:27 -0500

    mensaje

---
+++ src/main/java/cib/edu/pe/appwebfragments/AppWebFragmentsApplication.java
@@ -0,0 +1,13 @@
+package cib.edu.pe.appwebfragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFragmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cib/edu/pe/appwebfragments/AppWebFragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package cib.edu.pe.appwebfragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
